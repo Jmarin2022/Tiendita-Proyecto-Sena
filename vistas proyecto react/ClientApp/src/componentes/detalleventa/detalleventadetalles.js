75c9a14e77e1d7f695bd9ba045c1f9692ae3ff37
Juan Manuel <Juan Manuel@DESKTOP-2TJOV9Q>
2023-07-23 12:55:36 -0500

    terminado el modulo de ventas

--- vistas proyecto react/ClientApp/src/componentes/detalleventa/detalleventadetalles.js
+++ vistas proyecto react/ClientApp/src/componentes/detalleventa/detalleventadetalles.js
@@ -1,12 +1,42 @@
-﻿import { useEffect, useState } from "react";
+﻿import React, { useEffect, useState } from "react";
 import { NavBar } from '../principales/navbar';
 import '../../assets/css/menu.css';
+import { FaFilePdf } from 'react-icons/fa';
+import { PDFDownloadLink, Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+
+
+const styles = StyleSheet.create({
+    // Estilos para el PDF (los mismos que en tu código anterior)
+    page: {
+        flexDirection: 'column',
+        backgroundColor: '#ffffff',
+        padding: 20,
+    },
+    section: {
+        marginBottom: 10,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    subTitle: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    content: {
+        fontSize: 12,
+        marginBottom: 3,
+    },
+});
 
 export function ListadoDetalleventa(props) {
     const [detalleventa, setDetalleVenta] = useState([]);
     const [ventas, setVentas] = useState([]);
     const [ventaId, setVentaId] = useState(null);
-    const [imagenes, setImagenes] = useState([]); // Agregamos el estado para las imágenes
+    const [imagenes, setImagenes] = useState([]);
+    const [datosCargados, setDatosCargados] = useState(false); // Estado para controlar si los datos han sido cargados
 
     const obtenerDetallesVenta = async () => {
         const response = await fetch("api/detalleventa/Lista");
@@ -28,7 +58,7 @@ export function ListadoDetalleventa(props) {
         }
     };
 
-    const obtenerImagenes = async () => { // Agregamos la función para obtener las imágenes
+    const obtenerImagenes = async () => {
         const response = await fetch("api/imagen/Lista");
         if (response.ok) {
             const data = await response.json();
@@ -39,16 +69,21 @@ export function ListadoDetalleventa(props) {
     };
 
     useEffect(() => {
-        obtenerDetallesVenta();
-        obtenerVentas();
-        obtenerImagenes(); // Llamamos a la función para obtener las imágenes
+        const obtenerDatos = async () => {
+            await obtenerDetallesVenta();
+            await obtenerVentas();
+            await obtenerImagenes();
+            setDatosCargados(true);
+        };
+
+        obtenerDatos();
 
         const ventaIdFromUrl = window.location.pathname.split("/").pop();
         setVentaId(ventaIdFromUrl);
     }, []);
 
     const obtenerDetallesVentaComparados = () => {
-        if (!ventaId || detalleventa.length === 0 || ventas.length === 0 || imagenes.length === 0) {
+        if (!datosCargados) {
             return [];
         }
 
@@ -66,6 +101,69 @@ export function ListadoDetalleventa(props) {
     };
 
     const detallesVentaComparados = obtenerDetallesVentaComparados();
+
+    // Componente para el contenido del PDF
+    const generatePDF = async () => {
+        // Verificar si detallesVentaComparados tiene datos
+        if (detallesVentaComparados.length === 0) {
+            console.log("No hay datos para generar el PDF.");
+            return;
+        }
+
+        // Componente para el contenido del PDF
+        const DetalleVentaPDF = ({ detallesVentaComparados }) => (
+            <Document>
+                <Page size="A4" style={styles.page}>
+                    <View style={styles.section}>
+                        <Text style={styles.title}>Detalle de la Venta</Text>
+                        <Text style={styles.subTitle}>Fecha: {new Date().toLocaleString()}</Text>
+                        <Text style={styles.subTitle}>Venta ID: {ventaId}</Text>
+                    </View>
+                    <View style={styles.section}>
+                        {detallesVentaComparados.map((item) => (
+                            <Text key={item.id} style={styles.content}>
+                                {item.nombreProducto} - Cantidad: {item.cantidad} - Total: {item.total}
+                            </Text>
+                        ))}
+                    </View>
+                </Page>
+            </Document>
+        );
+
+        // Obtener el JSX del componente DetalleVentaPDF
+        const detalleVentaPDFInstance = (
+            <DetalleVentaPDF detallesVentaComparados={detallesVentaComparados} />
+        );
+
+        // Generar el Blob del PDF usando el componente PDFDownloadLink
+        const { blob } = await (
+            <PDFDownloadLink
+                document={detalleVentaPDFInstance}
+                fileName={`detalle_venta_${ventaId}.pdf`}
+            >
+                {({ blob, loading }) => (loading ? 'Generando PDF...' : blob)}
+            </PDFDownloadLink>
+        );
+
+        // Revocar el enlace de descarga anterior (si existe)
+        if (window.pdfDownloadLink) {
+            window.pdfDownloadLink.revokeObjectURL();
+        }
+
+        // Crear el enlace de descarga y descargar el PDF
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = `detalle_venta_${ventaId}.pdf`;
+        document.body.appendChild(link);
+        link.click();
+
+        // Guardar el enlace de descarga para poder revocarlo posteriormente
+        window.pdfDownloadLink = link;
+    };
+
+
+
+
 
     return (
         <div>
@@ -90,7 +188,7 @@ export function ListadoDetalleventa(props) {
                                 <tr>
                                     <th className="raya" scope="col">Id del detalle</th>
                                     <th className="raya" scope="col">Id de la venta</th>
-                                    <th className="raya" scope="col">Nombre del producto</th> {/* Cambiamos el encabezado */}
+                                    <th className="raya" scope="col">Nombre del producto</th>
                                     <th className="raya" scope="col">Cantidad</th>
                                     <th className="raya" scope="col">Total</th>
                                 </tr>
@@ -100,7 +198,7 @@ export function ListadoDetalleventa(props) {
                                     <tr key={item.id}>
                                         <td className="raya">{item.id}</td>
                                         <td className="raya">{item.ventaId}</td>
-                                        <td className="raya">{item.nombreProducto}</td> {/* Mostramos el nombre del producto */}
+                                        <td className="raya">{item.nombreProducto}</td>
                                         <td className="raya">{item.cantidad}</td>
                                         <td className="raya">{item.total}</td>
                                     </tr>
@@ -108,8 +206,14 @@ export function ListadoDetalleventa(props) {
                             </tbody>
                         </table>
                     </div>
+                    <div className="pdf-button">
+                        <button onClick={generatePDF}>
+                            Descargar PDF
+                        </button>
+                    </div>
                 </div>
             </div>
+            
         </div>
     );
 }
--- vistas proyecto react/ClientApp/src/componentes/detalleventa/detalleventaguardar.js
+++ vistas proyecto react/ClientApp/src/componentes/detalleventa/detalleventaguardar.js
@@ -4,6 +4,8 @@ import { NavBar } from '../principales/navbar';
 import '../../assets/css/menu.css';
 import { BiTrash } from 'react-icons/bi'; // Importar el icono de eliminación
 import { BiBrush } from 'react-icons/bi';
+import Swal from 'sweetalert2';
+
 
 export const ProductosFormulario = () => {
     const [productos, setProductos] = useState([]);
@@ -15,6 +17,7 @@ export const ProductosFormulario = () => {
     const [editIndex, setEditIndex] = useState(null);
     const [cliente, setCliente] = useState('');
     const [ventaId, setVentaId] = useState(null);
+    const [nombresProductosSeleccionados, setNombresProductosSeleccionados] = useState([]);
 
     useEffect(() => {
         const fetchProductos = async () => {
@@ -51,26 +54,51 @@ export const ProductosFormulario = () => {
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        const productoSeleccionado = productos.find((item) => item.nombre === producto.nombre);
+
+        if (!productoSeleccionado) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Producto no encontrado en la lista de productos disponibles.'
+            })
+            return;
+        }
+
+        const stockActual = productoSeleccionado.stock;
+
+        if (producto.cantidad > stockActual) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `No hay suficiente cantidad para comprar ${producto.cantidad} unidades del producto.`
+            })
+            return;
+        }
+
         if (editIndex !== null) {
             const updatedProductosSeleccionados = [...productosSeleccionados];
             updatedProductosSeleccionados[editIndex] = {
                 ...producto,
-                idImagen: productos.find((item) => item.nombre === producto.nombre).idImagen,
-                precio: productos.find((item) => item.nombre === producto.nombre).precio,
-                subtotal: producto.cantidad * productos.find((item) => item.nombre === producto.nombre).precio,
+                idImagen: productoSeleccionado.idImagen,
+                precio: productoSeleccionado.precio,
+                subtotal: producto.cantidad * productoSeleccionado.precio,
             };
             setProductosSeleccionados(updatedProductosSeleccionados);
             setEditIndex(null);
         } else {
             const newProducto = {
                 ...producto,
-                idImagen: productos.find((item) => item.nombre === producto.nombre).idImagen,
-                precio: productos.find((item) => item.nombre === producto.nombre).precio,
-                subtotal: producto.cantidad * productos.find((item) => item.nombre === producto.nombre).precio,
+                idImagen: productoSeleccionado.idImagen,
+                precio: productoSeleccionado.precio,
+                subtotal: producto.cantidad * productoSeleccionado.precio,
             };
             setProductosSeleccionados([...productosSeleccionados, newProducto]);
+
+            // Agregar el nombre del producto seleccionado a la lista de nombresProductosSeleccionados
+            setNombresProductosSeleccionados([...nombresProductosSeleccionados, producto.nombre]);
         }
         setProducto({
             cantidad: '',
@@ -109,39 +137,61 @@ export const ProductosFormulario = () => {
             const ventaResponse = await axios.post('/api/ventum/Guardar', ventaData);
             const ventaId = ventaResponse.data.idVenta;
             setVentaId(ventaId);
-            
+
+
+            const response1 = await axios.get('/api/ventum/Lista');
+            const idventa = response1.data[0].id;
+
             // Guardar los detalles de la venta
             productosSeleccionados.forEach(async (producto) => {
-                const response = await axios.get('/api/ventum/Lista');
-                const response1 = await axios.get('/api/imagen/Lista');
-
-
-                console.log(response); 
-                console.log(response1);
-                const hola = response.data[0].id
-
-                console.log(producto);
-
-                    
-
                 const detalleVentaData = {
-                    VentaId: hola,
+                    VentaId: idventa,
                     ProductoId: producto.idImagen,
                     Cantidad: producto.cantidad,
                     Total: producto.subtotal,
                 };
                 await axios.post('/api/detalleventa/Guardar', detalleVentaData);
-                window.location.href = "/venta";
+
+                // Actualizar el campo stock para el producto vendido
+                const idImagen = producto.idImagen;
+                const productoSeleccionadoAPI = productos.find((item) => item.idImagen === idImagen);
+
+                // Calcular el nuevo stock restando la cantidad vendida
+                const nuevoStock = productoSeleccionadoAPI.stock - producto.cantidad;
+
+                // Actualizar el campo stock en la API
+                await axios.put(`/api/imagen/Editar/${idImagen}`, {
+                    ...productoSeleccionadoAPI,
+                    stock: nuevoStock,
+                });
             });
 
             // Limpiar el formulario y el listado de productos seleccionados
             setCliente('');
             setProductosSeleccionados([]);
             setVentaId(null);
+            setNombresProductosSeleccionados([]); // Limpiar la lista de nombres de productos seleccionados
+
+            // Mostrar una alerta de éxito
+            Swal.fire({
+                icon: 'success',
+                title: 'Venta guardada correctamente.',
+                text: `El número de venta es: ${ventaId}`,
+            });
         } catch (error) {
             console.error(error);
+            // Mostrar una alerta de error
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al guardar la venta.',
+                text: 'Hubo un problema al intentar guardar la venta. Por favor, inténtalo nuevamente.',
+            });
         }
     };
+
+    // Filtra la lista de productos disponibles en el datalist para evitar mostrar los
+    // productos que ya han sido seleccionados previamente
+    const productosDisponibles = productos.filter((item) => !nombresProductosSeleccionados.includes(item.nombre));
 
     return (
         <div>
@@ -165,22 +215,30 @@ export const ProductosFormulario = () => {
                             <div className="form-group row">
                                 <label className="col-sm-3 col-form-label" htmlFor="cliente">Nombre del Cliente:</label>
                                 <div className="col-sm-9">
-                                    <input className="form-control " type="text" id="cliente" value={cliente} onChange={handleClienteChange} />
+                                    <input className="form-control" type="text" id="cliente" value={cliente} onChange={handleClienteChange} />
                                 </div>
                             </div>
                             <form onSubmit={handleSubmit}>
                                 <h2 className="letra1">Información del Producto para agregar</h2>
                                 <div className="row">
                                     <div className="form-row col-sm-5">
-                                        <label htmlFor="nombre">Nombre:</label>
-                                        <select class="form-select" id="nombre" name="nombre" value={producto.nombre} onChange={handleChange}>
-                                            <option value="">Seleccionar producto</option>
-                                            {productos.map((item, index) => (
+                                        <label htmlFor="cantidad">busque el nombre del producto:</label>
+                                        <input
+                                            className="form-control"
+                                            list="productosList"
+                                            id="nombre"
+                                            name="nombre"
+                                            value={producto.nombre}
+                                            onChange={handleChange}
+                                            placeholder="Buscar producto"
+                                        />
+                                        <datalist id="productosList">
+                                            {productosDisponibles.map((item, index) => (
                                                 <option key={index} value={item.nombre}>
                                                     {item.nombre}
                                                 </option>
                                             ))}
-                                        </select>
+                                        </datalist>
                                     </div>
 
                                     <div className="form-row col-sm-5">
@@ -188,12 +246,9 @@ export const ProductosFormulario = () => {
                                         <input className="form-control" type="number" id="cantidad" name="cantidad" value={producto.cantidad} onChange={handleChange} />
                                     </div>
 
-                                    <button class="btn btn-primary bajar1 col-sm" type="submit">{editIndex !== null ? 'Guardar Cambios' : 'Agregar Producto'}</button>
+                                    <button className="btn btn-primary bajar1 col-sm" type="submit">{editIndex !== null ? 'Guardar Cambios' : 'Agregar Producto'}</button>
                                 </div>
-                                
-                                
                             </form>
-                            
 
                             <h2 className="letra1">Listado de Productos</h2>
                             <table className="table1">
@@ -220,20 +275,20 @@ export const ProductosFormulario = () => {
                                                 </button>
                                             </td>
                                         </tr>
-                                ))}
-                                        </tbody>
+                                    ))}
+                                </tbody>
 
-                            {productosSeleccionados.length > 0 && (
-                                <div>
-                                    <p>Total: {productosSeleccionados.reduce((acc, item) => acc + item.subtotal, 0)}</p>
+                                {productosSeleccionados.length > 0 && (
+                                    <div>
+                                        <p>Total: {productosSeleccionados.reduce((acc, item) => acc + item.subtotal, 0)}</p>
                                         <button className="btn btn-primary bajar1" onClick={handleGuardarVenta}>Guardar Venta</button>
-                                </div>
-                            )}
-                                        
-                       </table> 
-            
-        </div> </div></div></div></div>
+                                    </div>
+                                )}
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
-
-
